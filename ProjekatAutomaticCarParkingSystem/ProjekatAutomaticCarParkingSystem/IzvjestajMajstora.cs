fcf27c9f41ffb2b2a70a327f357c9427078b8c7b
Merima Hadziabdic <mhadziabdi1@etf.unsa.ba>
2016-05-21 11:53:16 +0200

    Dodana klasa izvjestaj majstora

---
+++ ProjekatAutomaticCarParkingSystem/ProjekatAutomaticCarParkingSystem/IzvjestajMajstora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatAutomaticCarParkingSystem
+{
+    class IzvjestajMajstora
+    {
+        String opisKvara;
+        String materijal;
+        String uputstvoZaRukovanje;
+        String opisPopravke;
+        public String OpisKvara { get { return opisKvara; } set { opisKvara = value; } }
+        public String Materijal { get { return materijal; } set { materijal = value; } }
+        public String UputsvoZaRukovanje { get { return uputstvoZaRukovanje; } set { uputstvoZaRukovanje = value; } }
+        public String OpisPopravke { get { return opisPopravke; } set { opisPopravke = value; } }
+        public IzvjestajMajstora()
+        {
+
+        }
+        public IzvjestajMajstora(String _opisKvara,String _materijal,String _uputsvoZaRukovanje, String _opisPopravke)
+        {
+            opisKvara = _opisKvara;
+            opisPopravke = _opisPopravke;
+            materijal = _materijal;
+            uputstvoZaRukovanje = _uputsvoZaRukovanje;
+        }
+    }
+}
